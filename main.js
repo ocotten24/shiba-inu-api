8d76a22c135cb4d35161c367a52ebf1d3c357a1d
Owen <ocotten24@basecampcodingacademy.org>
2023-10-02 08:46:38 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+function displayImage(imageUrl) {
+    const imgElement = document.getElementById('dogimage');
+    imgElement.src = imageUrl;
+}
+
+
+function loadNewImage() {
+    fetch(`http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Error Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.length > 0) {
+          displayImage(data[0]);
+        } else {
+          console.error('No images found.');
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+}
+
+
+const loadNewImageButton = document.getElementById('loadNewImage');
+if (loadNewImageButton) {
+    loadNewImageButton.addEventListener('click', loadNewImage);
+}
+
+
+loadNewImage();
